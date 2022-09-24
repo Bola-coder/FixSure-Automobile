d3f65dbf44c05a92c83a4e87e0c5fa3b5311290e
Ahmed Bolarinwa <bolarinwaahmed22@gmail.com>
2022-09-24 22:18:42 +0100

    Worked on the BanneerCTA

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FirstBanner from "./FirstBanner";
-import styles from './../../Styles/Banner/Banner.module.css';
+import styles from "./../../Styles/Banner/Banner.module.css";
+import BannerCTA from "./BannerCTA";
 
 const Banner = () => {
   return (
     <section className={styles.banner}>
       <FirstBanner />
+      <BannerCTA />
     </section>
   );
 };
---
+++ src/components/Banner/BannerCTA.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./../../Styles/Banner/BannerCTA.module.css";
+
+const bannerCTA = () => {
+  return (
+    <div className={styles.bannerCta}>
+      <p className={styles.bannerCta__text}>
+        We understand your automobile needs
+      </p>
+      <button className={styles.bannerCta__button}>REQUEST QUOTE</button>
+    </div>
+  );
+};
+
+export default bannerCTA;
